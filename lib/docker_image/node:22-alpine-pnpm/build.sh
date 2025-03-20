9d8fe9d4ed7767049fd9b76c919826970c861b5f
yuchuan <13186087857@163.com>
2025-03-21 00:43:20 +0800

    fix

--- lib/docker_image/node:22-alpine-pnpm/build.sh
+++ lib/docker_image/node:22-alpine-pnpm/build.sh
@@ -1,42 +1,27 @@
-
-
-build_image() {
-}
-
-build_image
-push_image
-
-
 #!/bin/bash
 set -e
+set -o errexit
+set -o pipefail
 
+CURRENT_DIR=$(dirname $(realpath $0))
 WORKSPACE="$(dirname $(dirname $(dirname $(dirname $(realpath $0)))))"
 . "${WORKSPACE}"/lib/log.sh
 . "${WORKSPACE}"/lib/docker_tools.sh
 . "${WORKSPACE}"/etc/docker_config.sh
+. "${WORKSPACE}"/lib/os.sh
+. "${WORKSPACE}"/lib/check.sh
 
 . "${WORKSPACE}"/lib/param.sh
 analyze_params $*
 
 
-mkdir -p ./tmp
-if [ -f ${WORKSPACE}/etc/requirements.txt ]; then
-  cp -a ${WORKSPACE}/etc/requirements.txt ./tmp/requirements.txt
-fi
+DOCKER_IMAGE_NAME="${HARBOR_PREFIX}node"
+DOCKER_IMAGE_TAG="22-alpine-pnpm"
 
-DOCKER_IMAGE_NAME="${HARBOR_PREFIX}node:22-alpine-pnpm"
 build_image() {
-  docker images | grep "${HARBOR_PREFIX}ubuntu:20.04_python3.8" || die "docker image ${HARBOR_PREFIX}ubuntu:20.04_python3.8 not found"
-
-
-  docker build --build-arg BASE_IMAGE="${DOCKER_PROXY}library/node:22-alpine" -t "${DOCKER_IMAGE_NAME}" . || die "docker build failed"
-
-
-  docker build --build-arg BASE_IMAGE="${HARBOR_PREFIX}ubuntu:20.04_python3.8" -t "${DOCKER_IMAGE_NAME}" . || die "docker build failed"
+  docker build --build-arg BASE_IMAGE="${DOCKER_PROXY}library/node:22-alpine" -t "${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}" . || die "docker build failed"
 }
 
 build_image
-
-rm -rf ./tmp/
-
-push_image
+push_image_with_manifest_for_arch "${DOCKER_IMAGE_NAME}" "${DOCKER_IMAGE_TAG}"
+# push_image
