94d7dcdcf6a80c61cb4c738439a3f25607f1375f
yuchuan <13186087857@163.com>
2025-03-20 23:46:23 +0800

    add 2 image

--- lib/docker_image/python:3.10_django/build.sh
+++ lib/docker_image/python:3.10_django/build.sh
@@ -17,13 +17,20 @@ analyze_params $*
 get_os_arch
 cp -a ${CURRENT_DIR}/root/etc/apt/sources.list.${OS_ARCH} ${CURRENT_DIR}/root/etc/apt/sources.list
 
-DOCKER_IMAGE_NAME="${HARBOR_PREFIX}ubuntu"
-DOCKER_IMAGE_TAG="22.04_python3.10"
+DOCKER_IMAGE_NAME="${HARBOR_PREFIX}python"
+DOCKER_IMAGE_TAG="3.10_django"
+
+mkdir -p ./tmp
+if [ -f ${WORKSPACE}/etc/requirements.txt ]; then
+  cp -a ${WORKSPACE}/etc/requirements.txt ./tmp/requirements.txt
+fi
 
 build_image() {
-  docker build --build-arg BASE_IMAGE="${DOCKER_PROXY}library/ubuntu:22.04" -t "${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}" . || die "docker build failed"
+  docker build --build-arg BASE_IMAGE="${DOCKER_PROXY}yc913344706/ubuntu:22.04_python3.10" -t "${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}" . || die "docker build failed"
 }
 
 build_image
 push_image_with_manifest_for_arch "${DOCKER_IMAGE_NAME}" "${DOCKER_IMAGE_TAG}"
 # push_image
+
+rm -rf ./tmp/
